f1c8d96d22f587927e08ea262a8f9dc7246fe46a
xclcok <elhammoui.mousaab@gmail.com>
2024-01-04 17:22:14 +0100

    comm

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello from spring boot application :)";
+        return "Hello Mousaab from spring boot application :)";
     }
 
     @GetMapping("/user")
